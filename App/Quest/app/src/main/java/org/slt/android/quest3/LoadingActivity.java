216cad2ac4b3a92de672e7ef4e47ec2194893809
bluesquanium <bluesquanium@gmail.com>
2020-02-01 23:49:49 +0900

    image set front

--- App/Quest/app/src/main/java/org/slt/android/quest3/LoadingActivity.java
+++ App/Quest/app/src/main/java/org/slt/android/quest3/LoadingActivity.java
@@ -3,12 +3,16 @@ package org.slt.android.quest3;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class LoadingActivity extends Activity {
+    ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        logo = (ImageView)findViewById(R.id.logo);
+        logo.bringToFront();
         startLoading();
     }
     private void startLoading() {
