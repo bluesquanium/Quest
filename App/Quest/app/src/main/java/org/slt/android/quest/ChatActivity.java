5a16b6179c48b6fe1cdd8058afa09a36dff52c7b
bluesquanium <bluesquanium@gmail.com>
2020-02-08 14:12:14 +0900

    androidx, map

--- App/Quest/app/src/main/java/org/slt/android/quest/ChatActivity.java
+++ App/Quest/app/src/main/java/org/slt/android/quest/ChatActivity.java
@@ -2,8 +2,9 @@ package org.slt.android.quest;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ChatActivity extends AppCompatActivity {
     public static final int MYACITIVITY = Const.ACTIVITYCHAT;
--- App/Quest/app/src/main/java/org/slt/android/quest/CreateAccountActivity.java
+++ App/Quest/app/src/main/java/org/slt/android/quest/CreateAccountActivity.java
@@ -2,9 +2,10 @@ package org.slt.android.quest;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAccountActivity extends AppCompatActivity {
     @Override
